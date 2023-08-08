cb65b30db124fe2f1b4022a8ab85385de51d9a2f
Siimas <simaomarques20@gmail.com>
2023-08-08 20:34:39 +0100

    initial commit: started dapp implementation

---
+++ dapp/app/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import Navbar from "@/components/ui/navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
--- web/frontend/teixoso-token/app/components/Teste.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Teste() {
-
-  return (
-    <div>
-      ola
-    </div>
-  )
-}
